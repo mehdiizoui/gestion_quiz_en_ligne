9cd5ff8a2259cd7ac5879844215c9eb9052ecbce
USSEF98 <attarikyoussef8@gmail.com>
2021-01-13 17:25:44 +0000

    Upload Projet Avec les entitys

---
+++ src/main/java/org/upf/gestion_quiz/gestion_quiz_enligne/App.java
@@ -0,0 +1,13 @@
+package org.upf.gestion_quiz.gestion_quiz_enligne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/upf/gestion_quiz/gestion_quiz_enligne/AppTest.java
@@ -0,0 +1,20 @@
+package org.upf.gestion_quiz.gestion_quiz_enligne;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
